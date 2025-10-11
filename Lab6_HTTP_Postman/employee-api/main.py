6fef524f247c3e4ecc7d87f94023159b1081a662
user-pc <viktor.barbinov@student.karazin.ua>
2025-10-12 00:05:48 +0300

    Add global error handling middleware and exception handlers

--- Lab6_HTTP_Postman/employee-api/main.py
+++ Lab6_HTTP_Postman/employee-api/main.py
@@ -1,6 +1,14 @@
 import fastapi as FastAPI
+from src.middlewares import error_handler
 
 app = FastAPI.FastAPI()
 
 # додаємо маршрути з файлу employee.py
 app.include_router(employee.router)
+
+
+# Додаємо middleware
+app.add_middleware(error_handler.ErrorHandlerMiddleware)
+
+# Реєструємо глобальний exception handler
+error_handler.setup_exception_handlers(app)
